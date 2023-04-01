c7eea694eb26005596a2c011d9c8b3aa29809805
SanchesVitor <vitorsanches06@hotmail.com>
2023-04-01 16:29:52 -0300

    update script

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,8 @@ function ativaLetra(elemento){
     setTimeout(() => {
       elemento.innerHTML += letra;
     }, 75 * i);
-  })
+  });
 }
 
 const titulo = document.querySelector('.digitando');
+ativaLetra(titulo);
