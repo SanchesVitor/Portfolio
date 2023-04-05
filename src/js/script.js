5690753fa50004473888b120ae22fc9ab6999393
SanchesVitor <vitorsanches06@hotmail.com>
2023-04-04 23:01:04 -0300

    updated media querys and scripts

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,20 @@
-function ativaLetra(elemento){
-  const arrTexto = elemento.innerHTML.split('');
-  elemento.innerHTML = '';
-  arrTexto.forEach((letra, i) => {
-    setTimeout(() => {
-      elemento.innerHTML += letra;
-    }, 75 * i);
-  });
+function escrevendoLetra() {
+  function ativaLetra(elemento){
+    const arrTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrTexto.forEach((letra, i) => {
+      setTimeout(() => {
+        elemento.innerHTML += letra;
+      }, 75 * i);
+    });
+  }
+  
+  const titulo = document.querySelector('.digitando');
+  ativaLetra(titulo);
 }
 
-const titulo = document.querySelector('.digitando');
-ativaLetra(titulo);
+const ativaMenu = document.querySelector('.fa-bars');
+
+ativaMenu.addEventListener('click', () => {
+  ativaMenu.classList.toggle('fa-x');
+});
