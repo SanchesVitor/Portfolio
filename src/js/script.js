0d91d0ee0547a453c5c1579228c2763a8f8096d3
SanchesVitor <vitorsanches06@hotmail.com>
2023-04-05 22:13:14 -0300

    updated styles and scripts

--- src/js/script.js
+++ src/js/script.js
@@ -14,7 +14,9 @@ function escrevendoLetra() {
 }
 
 const ativaMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .navegacao-primaria');
 
 ativaMenu.addEventListener('click', () => {
   ativaMenu.classList.toggle('fa-x');
+  navMenu.classList.toggle('ativado');
 });
